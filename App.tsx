a3c533cb4d50cea1b63df498134500ca7ecb042f
Sterzinha <stermiss2@gmail.com>
2024-02-19 19:32:56 -0400

    Commit: CSS-in-JS-Definindo o tema da aplicaçao

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import { Groups } from "./src/screens/Groups";
+import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
+import { ThemeProvider } from "styled-components";
 
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
 
